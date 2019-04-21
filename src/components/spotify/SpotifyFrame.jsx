337adcde78e899f861a46785c65d7cd42ab4d778
crileyy <criley0007@gmail.com>
2019-04-21 12:22:39 -0700

    Adjusted iframe size

--- src/components/spotify/SpotifyFrame.jsx
+++ src/components/spotify/SpotifyFrame.jsx
@@ -5,7 +5,7 @@ export const SpotifyFrame = () => {
   return (
     <div className="spotify-frame">
       <span>Spotify iframe here</span>
-      <iframe title="spotify" src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe className="spotify" title="spotify" src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" height="500" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
   );
 };
